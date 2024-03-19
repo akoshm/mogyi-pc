c5a5a00c7b3ae553d33f15dc49deb48c5749979b
akoshm <mogyorosi98@gmail.com>
2024-03-19 23:52:49 +0100

    fix: "content" div fills in all available space add: react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Landing from './components/LandingPage'
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Landing />} />
+    </Routes>
   );
 }
 
